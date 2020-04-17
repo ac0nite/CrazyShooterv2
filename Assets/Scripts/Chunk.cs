32a3b7f14b593c8ae68fadcdf577c880c72a04c9
ac0nite <a.sachuk@gmail.com>
2020-04-17 20:15:44 +0300

    no message

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -9,10 +9,12 @@ public class Chunk : MonoBehaviour
 
     public List<ChunkDockPoint> DockPoints { get { return _dockPointSettings; } }
     public List<EnemySpawnPoint> EnemiesSpawnPoint { get { return _enemiesSpawnPoint; } }
+    public List<BoxCollider> BoundingColliders { get { return _boundingColliders; }  }
 
     [SerializeField] private List<ChunkDockPoint> _dockPointSettings = null;
     [SerializeField] private List<EnemySpawnPoint> _enemiesSpawnPoint = null;
-     
+    [SerializeField] private List<BoxCollider> _boundingColliders = null;
+
     public readonly Dictionary<ChunkDockPoint, Chunk> NeighbourChunks = new Dictionary<ChunkDockPoint, Chunk>();
      
     private void OnTriggerEnter(Collider other)
--- Assets/Scripts/LevelGenerationController.cs
+++ Assets/Scripts/LevelGenerationController.cs
@@ -45,26 +45,63 @@ public class LevelGenerationController : MonoBehaviour
         {
             if (center.NeighbourChunks.ContainsKey(centerDockPoint))
                 continue;
- 
-            var neighbour = GenerateRandomChunk(centerDockPoint.Type);
 
-            var neighbourDockPoint = neighbour.DockPoints.Find(p => p.Type == centerDockPoint.Type);
+            var suitableChunks = centerDockPoint.Type != ChunkDockPointType.Undefined
+                ? SettingsManager.Instance.Chunks.FindAll(c => c.DockPoints.Exists(t => t.Type == centerDockPoint.Type))
+                : SettingsManager.Instance.Chunks;
 
-            //Quaternion target = Quaternion.LookRotation(-centerDockPoint.transform.forward, centerDockPoint.transform.up);
-            //Quaternion source = Quaternion.LookRotation(neighbourDockPoint.transform.forward, neighbourDockPoint.transform.up);
-            //var anglesRotation = target.eulerAngles - source.eulerAngles;
-            //neighbour.transform.rotation *= Quaternion.Euler(anglesRotation);
+            while(suitableChunks.Count > 0)
+            {
+                var randomIndex = UnityEngine.Random.Range(0, suitableChunks.Count);
+                var prefab = suitableChunks[randomIndex];
+                suitableChunks.RemoveAt(randomIndex);
 
-            var target = Quaternion.LookRotation(-centerDockPoint.transform.forward, centerDockPoint.transform.up);
-            var rotationOffset = target * Quaternion.Inverse(neighbourDockPoint.transform.rotation);
-            neighbour.transform.rotation *= rotationOffset;
+                var neighbour = Instantiate(prefab);
 
-            //выровнять соседа относительно центрального
-            var offset = centerDockPoint.transform.position - neighbourDockPoint.transform.position;
-            neighbour.transform.position += offset;
+                var neighbourDockPoint = neighbour.DockPoints.Find(p => p.Type == centerDockPoint.Type);
 
-            center.NeighbourChunks[centerDockPoint] = neighbour;
-            neighbour.NeighbourChunks[neighbourDockPoint] = center;
+                var target = Quaternion.LookRotation(-centerDockPoint.transform.forward, centerDockPoint.transform.up);
+                var rotationOffset = target * Quaternion.Inverse(neighbourDockPoint.transform.rotation);
+                neighbour.transform.rotation *= rotationOffset;
+
+                //выровнять соседа относительно центрального
+                var offset = centerDockPoint.transform.position - neighbourDockPoint.transform.position;
+                neighbour.transform.position += offset;
+
+                if(IsEnoughSpaceForChunk(neighbour))
+                {
+                    EnemiesSpawn(neighbour);
+                    neighbour.EventPlayerEntered += OnPlayerEnteredChunk;
+                    center.NeighbourChunks[centerDockPoint] = neighbour;
+                    neighbour.NeighbourChunks[neighbourDockPoint] = center;
+                    break;
+                }
+                else
+                {
+                    Destroy(neighbour.gameObject);
+                }
+            }
+
+
+            //var neighbour = GenerateRandomChunk(centerDockPoint.Type);
+
+            //var neighbourDockPoint = neighbour.DockPoints.Find(p => p.Type == centerDockPoint.Type);
+
+            ////Quaternion target = Quaternion.LookRotation(-centerDockPoint.transform.forward, centerDockPoint.transform.up);
+            ////Quaternion source = Quaternion.LookRotation(neighbourDockPoint.transform.forward, neighbourDockPoint.transform.up);
+            ////var anglesRotation = target.eulerAngles - source.eulerAngles;
+            ////neighbour.transform.rotation *= Quaternion.Euler(anglesRotation);
+
+            //var target = Quaternion.LookRotation(-centerDockPoint.transform.forward, centerDockPoint.transform.up);
+            //var rotationOffset = target * Quaternion.Inverse(neighbourDockPoint.transform.rotation);
+            //neighbour.transform.rotation *= rotationOffset;
+
+            ////выровнять соседа относительно центрального
+            //var offset = centerDockPoint.transform.position - neighbourDockPoint.transform.position;
+            //neighbour.transform.position += offset;
+
+            //center.NeighbourChunks[centerDockPoint] = neighbour;
+            //neighbour.NeighbourChunks[neighbourDockPoint] = center;
 
             //check for neighbour
         }
@@ -72,6 +109,25 @@ public class LevelGenerationController : MonoBehaviour
         //BindNeighbourDockPointChanks(curent_chunk);
     }
 
+    private bool IsEnoughSpaceForChunk(Chunk chunk)
+    {
+        foreach(var boundingCollider in chunk.BoundingColliders)
+        {
+            //var center = boundingCollider.transform.position +  boundingCollider.transform.TransformPoint(boundingCollider.center);
+            var center = boundingCollider.transform.position;
+            var selColliders = Physics.OverlapBox(
+                center, 
+                boundingCollider.size / 2f, 
+                boundingCollider.transform.rotation, 
+                LayerMask.GetMask("Chunk"),
+                QueryTriggerInteraction.Collide);
+
+            if (selColliders.Length != 0)
+                return false;
+        }
+
+        return true;
+    }
     private void EnemiesSpawn(Chunk chunk)
     {
         foreach(var spawnPosition in chunk.EnemiesSpawnPoint)
